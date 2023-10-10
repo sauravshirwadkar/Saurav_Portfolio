32cf7ae210356a50fb55524a0426af9eab1d958f
Saurav Shirwadkar <saurav.shirwadkar@gmail.com>
2023-10-10 19:22:47 +0530

    description updated & minor UI changes

---
+++ js/saurav.js
@@ -0,0 +1,45 @@
+// Get a reference to the image element by its ID
+var imageElement1 = document.getElementById("img1");
+// Add a click event listener to the image
+  imageElement1.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+
+
+var imageElement2 = document.getElementById("img2");
+// Add a click event listener to the image
+imageElement2.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+
+var imageElement3 = document.getElementById("img3");
+// Add a click event listener to the image
+imageElement3.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+
+var imageElement4 = document.getElementById("img4");
+// Add a click event listener to the image
+imageElement4.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+var imageElement5 = document.getElementById("img5");
+// Add a click event listener to the image
+imageElement5.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+var imageElement6 = document.getElementById("img6");
+// Add a click event listener to the image
+imageElement6.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+var imageElement7 = document.getElementById("img7");
+// Add a click event listener to the image
+imageElement7.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
